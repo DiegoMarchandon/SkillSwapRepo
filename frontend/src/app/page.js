ea6c577cb2da6639fe82e3991495f7f0a891a08e
DiegoMarchandon <diegomarc2020@gmail.com>
2025-09-07 23:58:56 -0300

    instalacion de framer motion

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,9 +1,20 @@
-// import Image from "next/image";
+"use client";
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Plantilla limpia</h1>
+    <div style={{display:"flex", backgroundColor:"000000",justifyContent:"center",alignItems:"center",width:"100%",height:"100%",gap:20}}>
+      <h1>Plantilla </h1>
+      <motion.div
+      style={{
+        width:100,
+        height:100,
+        backgroundColor:"#ff0088",
+        borderRadius:5
+      }}
+      animate={{rotate:360}}
+      transition={{duration:2}}
+      />
     </div>
   );
 }
