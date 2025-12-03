936560f979fb0105c495e6b84f00267b090508ca
DiegoMarchandon <diegomarc2020@gmail.com>
2025-12-02 22:13:11 -0300

    reemplazando const pc para permitir más servidores STUN/TURN válidos

--- frontend/src/app/webrtc/WebrtcClient.js
+++ frontend/src/app/webrtc/WebrtcClient.js
@@ -291,7 +291,19 @@ export default function WebrtcClient() {
           const stream = await getLocalMedia();
 
           const pc = new RTCPeerConnection({
-            iceServers: [{ urls: 'stun:stun.l.google.com:19302' }],
+            iceServers: [
+              { urls: 'stun:stun.l.google.com:19302' },
+              {
+                urls: 'turn:openrelay.metered.ca:80',
+                username: 'openrelayproject',
+                credential: 'openrelayproject',
+              },
+              {
+                urls: 'turn:openrelay.metered.ca:443',
+                username: 'openrelayproject',
+                credential: 'openrelayproject',
+              },
+            ],
           });
           pcRef.current = pc;
 
